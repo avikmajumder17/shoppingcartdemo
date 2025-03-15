31cc172e02bc7bd47f542ca98b070199eb3b4f6b
Avik Majumder <avikmajumder17@gmail.com>
2025-03-15 14:30:20 +0530

    Project Shopppingcartdemo

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,18 @@
 export const Card = ({product}) => {
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
+      <a href="/">
         <img className="p-8 rounded-t-lg w-full" src={product.imgSrc} alt={product.name} />
       </a>
       
       <div className="px-5 pb-5">
-        <a href="#">
+        <a href="/">
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
         </a>
         
         <div className="flex items-center justify-between mt-4">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
-          <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+          <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
       </div>
     </div>    
